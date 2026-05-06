64c96c1b0245b634d9c22307fd0b58651813c6ac
Gert07 <puuramartin@gmail.com>
2026-05-06 13:22:15 +0300

    push

---
+++ ReactApp1/ReactApp1.Server/Controllers/PlanetsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReactApp1.Server.Controllers
+{
+    public class PlanetsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
